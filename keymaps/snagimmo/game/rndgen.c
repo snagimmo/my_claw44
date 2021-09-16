da5d4c082eee514f78a198b5bbdd5d0aea2bae11
snagimmo <31765952+snagimmo@users.noreply.github.com>
2021-09-17 01:15:56 +0900

    tweaked

--- keymaps/snagimmo/game/rndgen.c
+++ keymaps/snagimmo/game/rndgen.c
@@ -13,7 +13,7 @@ void init_rng(Rndgen *r, uint8_t x, uint8_t y, uint8_t z, uint8_t a, uint8_t t) 
 }
 
 uint8_t calc_rng(Rndgen *r, uint16_t p) {
-    // p contains the timer which is alternative to a random number
+    // p contains a timer variable which is alternative to a random number
     r->y = p;
     r->z = p;
 
