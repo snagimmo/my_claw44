fdc5c10698cd99b192a44ea8125d3a523157207c
snagimmo <31765952+snagimmo@users.noreply.github.com>
2021-09-16 21:11:27 +0900

    organized directories

--- keymaps/snagimmo/keymap.c
+++ keymaps/snagimmo/keymap.c
@@ -17,7 +17,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include <stdio.h>
 
 #include "lib/glcdfont.c"
-#include "lib/layers.c"
+#include "layers.c"
 #include "font_block.h"
 
 #include "game/game.h"
